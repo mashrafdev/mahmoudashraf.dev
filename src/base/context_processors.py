2fde2487718c5020d2ba15e0fab096e55bc5e6d0
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2026-06-01 20:15:07 +0300

    more secure settings for prod

--- src/base/context_processors.py
+++ src/base/context_processors.py
@@ -1,9 +1,10 @@
-from src.search.forms import SearchForm
 from src.base.models import Webring
+from src.search.forms import SearchForm
 
 
 def global_search_form(request):
     return {"search_form": SearchForm(request.GET or None)}
+
 
 def webrings_context(request):
     return {"webrings": Webring.objects.all()}
