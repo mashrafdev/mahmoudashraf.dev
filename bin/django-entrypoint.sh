a26a08029b815c294f5d962b415af96ed7e85615
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2026-08-01 20:36:06 +0300

    use threads

--- bin/django-entrypoint.sh
+++ bin/django-entrypoint.sh
@@ -7,7 +7,8 @@ python manage.py migrate --noinput
 exec gunicorn config.wsgi \
   --workers "${WEB_CONCURRENCY:-3}" \
   --bind 0.0.0.0:8000 \
-  --worker-class sync \
+  --worker-class gthread \
+  --threads 4 \
   --timeout 60 \
   --access-logfile - \
   --error-logfile - \
